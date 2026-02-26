9db28820df6e898b831ed08c2292f7285d2d7dca
xpieemma <7chrispeter@gmail.com>
2026-02-25 19:33:27 -0500

    dashboard header

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -7,6 +7,18 @@ export default function Dashboard () {
 
     return (
         <>
+        <div>
+
+<div>
+<header>
+    
+    <h1 className="text-3xl font-bold text-slate-900">Task DashBoard </h1>
+
+</header>
+
+</div>
+
+        </div>
         </>
     );
 } 
