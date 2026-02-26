822fd3d8813bb71b9ca0de6af36de91afd105dca
xpieemma <7chrispeter@gmail.com>
2026-02-25 19:55:54 -0500

    fixed tailwind plugins in vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
 
   return (
     <>
+    <h1 className="text-4xl font-bold text-red-500">
+      Tailwind is working
+    </h1>
     <Dashboard />
 
     </>
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -7,14 +7,54 @@ export default function Dashboard () {
 
     return (
         <>
-        <div>
+        <div className="min-h-screen bg-slate-50 p-4 md:p-8">
 
-<div>
+<div className="max-w-7xl mx-auto space-y-6">
 <header>
     
     <h1 className="text-3xl font-bold text-slate-900">Task DashBoard </h1>
 
 </header>
+
+<div>
+
+
+    <div>
+
+<section>
+
+    <h2>
+
+        Filters
+    </h2>
+    <div>
+        TaskFilter Component
+    </div>
+</section>
+
+<section>
+    <h2>
+        Add Task
+    </h2>
+    <div>
+        TaskForm Component
+    </div>
+
+</section>
+    </div>
+    <div>
+        <section>
+            <div>
+                <h2>Tasks 
+
+                </h2>
+            </div>
+            <div>
+                TaskList Component
+            </div>
+        </section>
+    </div>
+</div>
 
 </div>
 
